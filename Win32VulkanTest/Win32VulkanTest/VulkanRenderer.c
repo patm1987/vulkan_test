c3ca891a34ec64d42327f6f9142d7088fc2b522b
Patrick Martin <pux1987@gmail.com>
2016-02-18 22:26:09 -0700

    I now find a physical device that I'd like to use
    The next step will be making a logical device

--- Win32VulkanTest/Win32VulkanTest/VulkanRenderer.c
+++ Win32VulkanTest/Win32VulkanTest/VulkanRenderer.c
@@ -6,6 +6,10 @@
 #define STR(s) #s
 #define PRINT_VK_RESULT(result) case result: printf( STRINGIFY(result) "\n"); break;
 
+struct vulkan_renderer_t {
+	VkInstance instance;
+};
+
 void PrintResult(VkResult result) {
 	printf("Vulkan Result: ");
 	switch (result) {
@@ -37,9 +41,33 @@ void PrintResult(VkResult result) {
 	}
 }
 
-struct vulkan_renderer_t {
-	VkInstance instance;
+// order the devices by the kind we'd prefer to run on
+VkPhysicalDeviceType aDevicePrecidents[] = {
+	VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU,
+	VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU,
+	VK_PHYSICAL_DEVICE_TYPE_VIRTUAL_GPU,
+	VK_PHYSICAL_DEVICE_TYPE_CPU,
+	VK_PHYSICAL_DEVICE_TYPE_OTHER,
 };
+
+/*!
+ * \brief	Determine if the \a newType of device is better than the \a oldType
+ * \param	newType the new type of device
+ * \param	oldType the old type of device
+ * \return	TRUE if we think that \a newType is better than or equal to \a oldType
+ */
+BOOL DeviceTypeIsSuperior(VkPhysicalDeviceType newType, VkPhysicalDeviceType oldType) {
+	for (int i = 0; i < sizeof(aDevicePrecidents) / sizeof(VkPhysicalDevice); i++) {
+		VkPhysicalDeviceType currentPrecident = aDevicePrecidents[i];
+		if (currentPrecident == newType) {
+			return TRUE;
+		}
+		if (currentPrecident == oldType) {
+			return FALSE;
+		}
+	}
+	return TRUE;
+}
 
 VulkanRenderer* VulkanRenderer_Create() {
 	VulkanRenderer* pVulkanRenderer = (VulkanRenderer*)malloc(sizeof(VulkanRenderer*));
@@ -63,8 +91,76 @@ VulkanRenderer* VulkanRenderer_Create() {
 	}
 
 	// get all the physical devices
+
+	// first find the numer of devices
+	uint32_t physicalDeviceCount;
+	result = vkEnumeratePhysicalDevices(
+		pVulkanRenderer->instance,
+		&physicalDeviceCount,
+		NULL);
+	if (result != VK_SUCCESS) {
+		PrintResult(result);
+		exit(1);
+	}
+
+	// then get the devices
+	VkPhysicalDevice* paPhysicalDevices = (VkPhysicalDevice*)malloc(
+		sizeof(VkPhysicalDevice) * physicalDeviceCount);
+	result = vkEnumeratePhysicalDevices(
+		pVulkanRenderer->instance,
+		&physicalDeviceCount,
+		paPhysicalDevices);
+
 	// find one we like
+	VkPhysicalDevice chosenDevice = NULL;
+	VkPhysicalDeviceProperties chosenDeviceProperties;
+	for (uint32_t deviceIndex = 0; deviceIndex < physicalDeviceCount; deviceIndex++) {
+		VkPhysicalDeviceProperties deviceProperties;
+		vkGetPhysicalDeviceProperties(paPhysicalDevices[deviceIndex], &deviceProperties);
+
+		uint32_t queueFamilyPropertyCount;
+		vkGetPhysicalDeviceQueueFamilyProperties(
+			paPhysicalDevices[deviceIndex],
+			&queueFamilyPropertyCount,
+			NULL);
+		VkQueueFamilyProperties* paQueueFamilyProperties
+			= (VkQueueFamilyProperties*)malloc(
+				sizeof(VkQueueFamilyProperties) * queueFamilyPropertyCount);
+
+		BOOL queueValid = FALSE;
+		for (uint32_t queueIndex = 0; queueIndex < queueFamilyPropertyCount; queueIndex++) {
+			if (paQueueFamilyProperties[queueIndex].queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+				queueValid = TRUE;
+				break;
+			}
+		}
+
+		if (!chosenDevice) {
+			chosenDevice = paPhysicalDevices[deviceIndex];
+			chosenDeviceProperties = deviceProperties;
+		}
+		else {
+			if (DeviceTypeIsSuperior(
+				deviceProperties.deviceType,
+				chosenDeviceProperties.deviceType)) {
+
+				chosenDevice = paPhysicalDevices[deviceIndex];
+				chosenDeviceProperties = deviceProperties;
+			}
+		}
+
+		// if we found a discreet GPU - that's probably ideal
+		if (chosenDeviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU) {
+			// we've struck gold!
+			break;
+		}
+
+		free(paQueueFamilyProperties);
+	}
+
 	// make a logical device
+
+	free(paPhysicalDevices);
 
 	return pVulkanRenderer;
 }
